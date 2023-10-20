05a9c2afe955c62ffb7aa2eb3ff9b12f7fcda8fb
juditperea <22jperea@ibadia.cat>
2023-10-20 12:57:32 +0200

    MongoDB Connection established, server not running

--- models.js
+++ models.js
@@ -1,5 +1,52 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/users', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('Conexión a MongoDB establecida');
+});
+
+const typeDefs = gql`
+  type User {
+    id: ID
+    username: String
+    name: String
+    surname: String
+    country: String
+  }
+
+  type Group {
+    id: ID
+    name: String
+    members: [User]
+  }
+
+  type Mutation {
+    createUser(username: String, name: String, surname: String, country: String): User
+    createGroup(name: String, memberIds: [String]): Group
+  }
+`;
+
+const resolvers = {
+    Mutation: {
+      createUser: async (_, { username, name, surname, country }) => {
+        const user = new UserModel({ username, name, surname, country });
+        await user.save();
+        return user;
+      },
+      createGroup: async (_, { name, memberIds }) => {
+        const members = await UserModel.find({ id: { $in: memberIds } });
+        const group = new GroupModel({ name, members });
+        await group.save();
+        return group;
+      },
+    },
+  };
+  
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -29,16 +76,21 @@ const groupSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  members: {
+  members: [{
     type: mongoose.Types.ObjectId,
     ref: 'User',
-  },
+  }],
 });
 
 const UserModel = mongoose.model('User', userSchema);
 const GroupModel = mongoose.model('Group', groupSchema);
 
-module.exports = {
-  UserModel,
-  GroupModel,
-};
+
+/*const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+);*/
